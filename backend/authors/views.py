c068424af56fb8fc894ac9931231ec185d8dde26
Gary Ng Kwong Sang <gng4820@gmail.com>
2023-10-13 20:56:46 -0600

    Setup pagination for authors endpoint

--- backend/authors/views.py
+++ backend/authors/views.py
@@ -2,14 +2,25 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Author
 from .serializers import AuthorSerializer
+from rest_framework.pagination import PageNumberPagination
 
-class AuthorList(APIView):
+class AuthorList(APIView, PageNumberPagination):
     """
     View to list all profiles on the server.
     """
-    ## TODO @garynks: Add pagination and size limit
     def get(self, request):
+        # Pagination settings
+        self.page_size_query_param = 'size'
+        self.page_size = 10 # default page size
+        self.max_page_size = 100
+
         authors = Author.objects.all()
+
+        # if a page query is provided, paginate the results
+        if self.get_page_number(request, self):
+            # results are paginated either by the default page size or the page size query parameter
+            if self.get_page_size(request):
+                authors = self.paginate_queryset(authors, request)
         serializer = AuthorSerializer(authors, many=True)
         return Response({
             "type": "authors",
