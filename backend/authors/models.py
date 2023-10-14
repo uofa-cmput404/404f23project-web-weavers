88d7b74911cb2fc11a752c873b93f1118d82fb18
Gary Ng Kwong Sang <gng4820@gmail.com>
2023-10-13 19:19:32 -0600

    Initialize Author Model

--- backend/authors/models.py
+++ backend/authors/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Author(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    type = models.CharField(max_length=50, default="author")
+    id = models.URLField(max_length=200, unique=True, editable=False)
+    host = models.URLField(max_length=200, default="http://127.0.0.1:8000/")
+    displayName = models.CharField(max_length=100)
+    url = models.URLField(max_length=200, unique=True, editable=False)
+    github = models.URLField(max_length=200, blank=True)
+    profileImage = models.URLField(max_length=100, blank=True)
+
+    def save(self, *args, **kwargs):
+        # When the object is instantiated, set the id and url fields using the host and uuid
+        if not self.id:
+            self.id = self.host + "authors/" + str(self.uuid)
+            self.url = self.host + "authors/" + str(self.uuid)
+        super().save(*args, **kwargs)
