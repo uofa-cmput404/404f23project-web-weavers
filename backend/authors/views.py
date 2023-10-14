008f251152dbcfd47f5c51ec882f1a57faf265d7
Gary Ng Kwong Sang <gng4820@gmail.com>
2023-10-13 19:23:42 -0600

    Added GET Handler logic for listing authors and getting author details

--- backend/authors/views.py
+++ backend/authors/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Author
+from .serializers import AuthorSerializer
 
-# Create your views here.
+class AuthorList(APIView):
+    """
+    View to list all profiles on the server.
+    """
+    ## TODO @garynks: Add pagination and size limit
+    def get(self, request):
+        authors = Author.objects.all()
+        serializer = AuthorSerializer(authors, many=True)
+        return Response({
+            "type": "authors",
+            "items": serializer.data
+        })
+
+class AuthorDetails(APIView):
+    """
+    View to retrieve or update a specific author's profile in the server.
+    """
+    def get(self, request, pk):
+        author = Author.objects.get(pk=pk)
+        serializer = AuthorSerializer(author)
+        return Response(serializer.data)
