b39a8f5028af1d1be9d114ca941038548b8dd553
Genna-C <genna@ualberta.ca>
2023-10-16 18:30:01 -0600

    add login and home page support

--- frontend/social-distribution/src/App.js
+++ frontend/social-distribution/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './login';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {useEffect, useState} from 'react';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = {"App"}>
+      <BrowserRouter>
+        <Routes>
+          <Route path = "/" element = {<Login setLoggedIn={setLoggedIn}/>} />
+          <Route path = "/home"/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
