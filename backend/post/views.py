40a7d98458cf66be41ddcd47334c9875e2fb06e9
sparsh_thakur <113547853+skullemote@users.noreply.github.com>
2023-10-19 13:27:08 -0600

    models registered to admin and serializer added

--- backend/post/views.py
+++ backend/post/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework.pagination import PageNumberPagination
+
+class PostList(APIView, PageNumberPagination):
+    
 
 # Create your views here.
+
+
