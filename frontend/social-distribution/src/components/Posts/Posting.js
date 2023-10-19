c03dc52a705070108c37315efab9147fa730a101
jazarcon <jd.azarcon3@gmail.com>
2023-10-18 22:15:29 -0600

    testing

--- frontend/social-distribution/src/components/Posts/Posting.js
+++ frontend/social-distribution/src/components/Posts/Posting.js
@@ -1,65 +1,74 @@
-import React, { useContext, useState, Component, View } from "react";
-import { useEffect } from "react";
-import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
+import React, { useState } from "react";
+import { 
+    Card, 
+    CardHeader, 
+    CardBody, 
+    Flex, 
+    Avatar, 
+    Box, 
+    Heading,
+    Divider,
+    Text
+} from '@chakra-ui/react'
 import "./Posting.css"
 
 export default function Post(props){
     const userID= localStorage.getItem()
     const postID = 1;                       // figure out how to get ID later
-    const [IsLiked, SetIsLiked]= useState(false);
-    const [postTitle, setPostTitle]= useState(false)
-    const [postcontent, SetpostContent]= useState(false)
+    const [IsLiked, SetIsLiked]= useState("");
+    const [postTitle, setPostTitle]= useState("")
+    const [postcontent, SetpostContent]= useState("")
     const [postImage, SetpostImage]= useState(props.post.image);
 
     // if no content in post- just an image
-    const [showContent, setShowContent] = useState(() => {
-        if (props.post.contentType.startsWith("image")) {
-          return false;
-        } else {
-          return true;
-        }
-    });
+    // const [showContent, setShowContent] = useState(() => {
+    //     if (props.post.contentType.startsWith("image")) {
+    //       return false;
+    //     } else {
+    //       return true;
+    //     }
+    // });
 
     // user information- communicate with the backend
     // async and await
-    useEffect(() => {
-        const getUserData = async() => {
-            await api
-            .get()              // get url from backend
-            .then((response) => {
+    // useEffect(() => {
+    //     const getUserData = async() => {
+    //         await api
+    //         .get()              // get url from backend
+    //         .then((response) => {
                 
-            })
+    //         })
 
 
-            await api
+    //         await api
 
-        }
+    //     }
 
 
-        const getPostImage = async() => {
-            await api
-            .get()
-            .then((res) => {
-                const data= ''
-                SetpostImage(data)
-            })
+    //     const getPostImage = async() => {
+    //         await api
+    //         .get()
+    //         .then((res) => {
+    //             const data= ''
+    //             SetpostImage(data)
+    //         })
 
-            .catch((err) => {
-                console.log(err)
-            })
-        }
+    //         .catch((err) => {
+    //             console.log(err)
+    //         })
+    //     }
 
-        const getPostTitle = async() => {
-            await api
-            .get()
-            .then((res) => {
-                const title=''
-                setPostTitle(title)
-            })
-        }
-    })
+    //     const getPostTitle = async() => {
+    //         await api
+    //         .get()
+    //         .then((res) => {
+    //             const title=''
+    //             setPostTitle(title)
+    //         })
+    //     }
+    // })
 
-    getUserData()
+    // getUserData()
 
     return(
         <Card>
@@ -68,7 +77,7 @@ export default function Post(props){
                     <Flex flex='1' gap='4' alignItems='center' flexWrap='wrap'>
                         <Avatar name='Richard' src='https://bit.ly/sage-adebayo' />
                         <Box>
-                            <Heading size='sm'>Richard</Heading>
+                            <Heading>Richard</Heading>
                         </Box>
                     </Flex>
                 </Flex>
@@ -77,26 +86,18 @@ export default function Post(props){
             <Divider/>
 
             <CardHeader>
-                <Heading size="lg">{postTitle}</Heading>
+                <Heading size="lg">Title</Heading>
             </CardHeader>
             <CardHeader>
-                <div className="card-content">
 
-                </div>
             </CardHeader>
             <CardBody>
-                <Image
                 
-                />
+                <Divider />
+
                 
-                <Diver />
 
-                <div className="like-comment-area">
-
-                    
-                </div>
-
-                <Text>{postcontent}</Text>
+                <Text>talking about stuff</Text>
 
             </CardBody>
         </Card>
--- frontend/social-distribution/src/home.js
+++ frontend/social-distribution/src/home.js
@@ -1,10 +1,12 @@
 import React, {useState} from 'react';
 import { useNavigate } from "react-router-dom";
+import Posting from './components/Posts/Posting'
 
 function Home() {
 
     return (<div className = {"mainContainer"}>
         <div> <div> Home Page </div> </div>
+        <Posting />
         </div>
     );
   }
