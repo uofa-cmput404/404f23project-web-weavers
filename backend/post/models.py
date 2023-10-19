1e44bef5a089901a37c76c8c21787219db48da0b
sparsh_thakur <113547853+skullemote@users.noreply.github.com>
2023-10-19 04:22:15 -0600

    models.py updated for post

--- backend/post/models.py
+++ backend/post/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+import uuid
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    type = models.CharField(max_length=200, default="post")
+    title = models.CharField(max_length=200)
+    source = models.URLField()
+    origin = models.URLField()
+    description = models.CharField(max_length=200)
+    contentType = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    categories = models.JSONField()
+    count = models.IntegerField()
+    comments = models.URLField()
+    published = models.DateTimeField()
+    visibility = models.CharField(max_length=255)
+    unlisted = models.BooleanField()
+
+    commentsSrc = models.JSONField(null=True, blank=True)
 
 # Create your models here.
