d1c3ec870881d53d10349f4048f8c21fca148b12
sparsh_thakur <113547853+skullemote@users.noreply.github.com>
2023-10-24 13:17:12 -0600

    changes in test

--- backend/post/tests.py
+++ backend/post/tests.py
@@ -10,35 +10,36 @@ class PostTests(TestCase):
 
     def test_get_post_list(self):
         client = APIClient()
-        response = client.get('/your-api-endpoint/')  # Replace with your actual API endpoint
+        #need to change this to the actual endpoint
+        response = client.get('')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['type'], 'posts')
 
     def test_get_post_detail(self):
         client = APIClient()
-        response = client.get(f'/your-api-endpoint/{self.post.id}/')  # Replace with your actual API endpoint and post ID
+        response = client.get(f'/your-api-endpoint/{self.post.id}/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['title'], self.post.title)
 
     def test_create_post(self):
         client = APIClient()
         data = {'title': 'New Post', 'content': 'New content'}
-        response = client.post('/your-api-endpoint/', data, format='json')  # Replace with your actual API endpoint
+        response = client.post('/your-api-endpoint/', data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Post.objects.count(), 2)  # Check that a new post was created
 
     def test_update_post(self):
         client = APIClient()
         data = {'title': 'Updated Post', 'content': 'Updated content'}
-        response = client.put(f'/your-api-endpoint/{self.post.id}/', data, format='json')  # Replace with your actual API endpoint and post ID
+        response = client.put(f'/your-api-endpoint/{self.post.id}/', data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(Post.objects.get(id=self.post.id).title, 'Updated Post')
 
     def test_delete_post(self):
         client = APIClient()
-        response = client.delete(f'/your-api-endpoint/{self.post.id}/')  # Replace with your actual API endpoint and post ID
+        response = client.delete(f'/your-api-endpoint/{self.post.id}/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(Post.objects.count(), 0)  # Check that the post was deleted
+        self.assertEqual(Post.objects.count(), 0)
 
 
 # Create your tests here.
