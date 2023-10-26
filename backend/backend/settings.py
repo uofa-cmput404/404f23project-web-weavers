7352e1a6f834a43f7018a3ed0adaa8de825eacef
sparsh_thakur <113547853+skullemote@users.noreply.github.com>
2023-10-26 01:04:10 -0600

    Changes made to models.py\

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "web-database",
         "USER": "postgres",
-        "PASSWORD": "12345",
+        "PASSWORD": "Sparsh28",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
--- backend/post/models.py
+++ backend/post/models.py
@@ -1,25 +1,43 @@
 from django.db import models
 import uuid
+from django.contrib.postgres.fields import ArrayField
+from django.utils import timezone
 from authors.models import Author
 
+# Create your models here.
+
 class Post(models.Model):
+    CT_MARKDOWN = 'text/markdown'
+    CT_PLAIN = 'text/plain'
+    CT_HTML = 'text/html'
+    CT_BASE64 = 'application/base64'
+    CT_PNG = 'image/png;base64'
+    CT_JPG = 'image/jpeg;base64'
+    CONTENT_TYPE_CHOICES = [
+        (CT_MARKDOWN, 'Markdown'),
+        (CT_PLAIN, 'Plain Text'),
+        (CT_HTML, 'HTML'),
+        (CT_BASE64, 'Base64 Encoded'),
+        (CT_PNG, 'PNG Image'),
+        (CT_JPG, 'JPEG Image'),
+    ]
     type = models.CharField(max_length=200, default="post")
     title = models.CharField(max_length=200)
     id = models.URLField(primary_key=True, default=uuid.uuid4, editable=False)     
     source = models.URLField()
     origin = models.URLField()
     description = models.CharField(max_length=200)
-    contentType = models.CharField(max_length=200)
-    content = models.TextField()
+    contentType = models.CharField(
+        max_length=200,
+        choices=CONTENT_TYPE_CHOICES,
+        default=CT_MARKDOWN
+        )
+    content = models.TextField(default="")
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    categories = models.ArrayField()
-    count = models.IntegerField()
-    comments = models.URLField()
+    categories = ArrayField(models.TextField(), blank=True, default=list, null=True)
+    count = models.PositiveIntegerField(default=0)
+    comments = models.URLField(null=True, blank=True)
     commentsSrc = models.JSONField(null=True, blank=True) # TODO: will be replaced by the Comment model when implemented
-    published = models.DateTimeField()
+    published = models.DateTimeField(default=timezone.now)
     visibility = models.CharField(max_length=255)
     unlisted = models.BooleanField()
-
-    
-
-# Create your models here.
