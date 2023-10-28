de124e91a3f9e1c5a8436c973a8c393ade1449f9
Gary Ng Kwong Sang <gng4820@gmail.com>
2023-10-27 20:00:05 -0600

    Rewrote Post views

--- backend/post/views.py
+++ backend/post/views.py
@@ -1,169 +1,102 @@
-from django.shortcuts import render, get_object_or_404
-from django.forms.models import model_to_dict
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from .models import Post
 from authors.models import Author
 from .serializers import PostSerializer
 from rest_framework.pagination import PageNumberPagination
+import uuid
 
 # Create your views here.
+class PostList(APIView, PageNumberPagination):
+    def get(self, request, author_id):
+        """
+        List all posts.
+        """
+        # Pagination settings
+        self.page_size_query_param = 'size'
+        self.page_size = 10 # default page size
+        self.max_page_size = 100
 
-@api_view(['GET', 'POST', 'PUT', 'DELETE'])
-def post_list(request, author_id=None, post_id=None):
-    """
-    List all posts, or create a new post.
-    """
-    if request.method == 'GET':
-        paginator = PageNumberPagination()
-        paginator.page_size = 10
-        
-        if author_id is not None:
-            if post_id is not None:
-                author = Author.objects.get(id=author_id)
-                post = Post.objects.get(id=post_id, author=author, visibility="PUBLIC")
-                serializer = PostSerializer(post)
-                serial_data = serializer.data
-                serial_data["id"] = url + f'/author/{post.author_id}/posts/{post_id}'
-                serial_data["author"] = {
-                    "type": post.author.type,
-                    "id": f'{url}/author/{post.author_id}',
-                    "host": post.author.host,
-                    "displayName": post.author.displayName,
-                    "url": post.author.url,
-                    "github": f'http://github.com/{post.author.githubName}',
-                    "profileImage": post.author.profileImage
-                        }
-                return Response(serial_data, status = status.HTTP_200_OK)
-            elif post_id is None:
-                author = Author.objects.get(id=author_id)
-                posts = Post.objects.filter(author=author, visibility="PUBLIC")
-                results = paginator.paginate_queryset(posts, request)
-                serializer = PostSerializer(results, many=True)
-                data = serializer.data
+        author = Author.objects.get(pk=author_id)
+        # get the latest public posts from the author
+        posts = Post.objects.filter(author=author, visibility="PUBLIC").order_by('-published')
 
-                for entry in data:
-                    entry["id"] = url + f'/author/{author_id}/posts/{entry["id"]}'
-                    entry["author"] = {
-                        "type": author.type,
-                        "id": f'{url}/author/{author_id}',
-                        "host": author.host,
-                        "displayName": author.displayName,
-                        "url": author.url,
-                        "github": f'http://github.com/{author.githubName}',
-                        "profileImage": author.profileImage
-                            }
-                    
-                return Response(data, status = status.HTTP_200_OK)
-        
-        elif author_id is None:
-            post_obj = Post.objects.filter(visibility="PUBLIC")
-            results = paginator.paginate_queryset(post_obj, request)
-            serializer = PostSerializer(results, many=True)
-            data = serializer.data
+        # if a page query is provided, paginate the results
+        if self.get_page_number(request, self):
+            # results are paginated either by the default page size or the page size query parameter
+            if self.get_page_size(request):
+                posts = self.paginate_queryset(posts, request)
 
-            for entry in data:
-                author = Author.objects.get(id=entry["author"])
-                entry["id"] = url + f'/author/{entry["author"]}/posts/{entry["id"]}'
-                entry["author"] = {
-                    "type": author.type,
-                    "id": f'{url}/author/{entry["author"]}',
-                    "host": author.host,
-                    "displayName": author.displayName,
-                    "url": author.url,
-                    "github": f'http://github.com/{author.githubName}',
-                    "profileImage": author.profileImage
-                        }
-                
-            return Response(data, status = status.HTTP_200_OK)
-                
-    elif request.method == 'POST':
-        if post_id is not None:
-            author = Author.objects.get(id=author_id)
-            post = Post.objects.get(id=post_id, author=author)
-            post_data = request.data
+        serializer = PostSerializer(posts, many=True)
+        return Response({
+            "type": "posts",
+            "items": serializer.data
+        })
+    
+    def post(self, request, author_id):
+        """
+        Create a new post with a new id.
+        """
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            author = Author.objects.get(pk=author_id)
+            serializer.validated_data["author"] = author
 
-            post.title = post_data["title"]
-            post.description = post_data["description"]
-            post.contentType = post_data["contentType"]
-            post.content = post_data["content"]
-            post.categories = post_data["categories"]
-            post.visibility = post_data["visibility"]
-
-            post.save(update_fields=["title", "description", "contentType", "content", "categories", "visibility"])
-
-            data = model_to_dict(post)
-            data["published"] = str(data["published"])
-            data["id"] = url + f'/author/{author_id}/posts/{post_id}'
-            data["author"] = {
-                "type": author.type,
-                "id": str(author.id),
-                "host": author.host,
-                "displayName": author.displayName,
-                "url": author.url,
-                "github": f'http://github.com/{author.githubName}',
-                "profileImage": author.profileImage
-                    }
-            return Response(data, status = status.HTTP_200_OK)
-        
-        elif post_id is None:
-            author = Author.objects.get(id=author_id)
-            post_data = request.data
-            post_data.update({"author": author.id})
-            serializer = PostSerializer(data=post_data)
-            serializer.is_valid(raise_exception=True)
+            post_url = author.url + "/posts/" + str(uuid.uuid4())
+            serializer.validated_data["id"] = post_url
+            serializer.validated_data["source"] = post_url
+            serializer.validated_data["origin"] = post_url
             serializer.save()
-            data = serializer.data
-            data["published"] = str(data["published"])
-            data["id"] = url + f'/author/{author_id}/posts/{data["id"]}'
-            data["author"] = {
-                "type": author.type,
-                "id": str(author.id),
-                "host": author.host,
-                "displayName": author.displayName,
-                "url": author.url,
-                "github": f'http://github.com/{author.githubName}',
-                "profileImage": author.profileImage
-                    }
-            return Response(data, status = status.HTTP_201_CREATED)
-        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'PUT':
-        if post_id is not None:
-            author = Author.objects.get(id=author_id)
-            post_data = request.data
-            post_data.update({"author": author.id, "id": post_id})
-            serializer = PostSerializer(data=post_data)
-            serializer.is_valid(raise_exception=True)
-            serializer.save(id=post_id)
-
-            data = serializer.data
-            data["published"] = str(data["published"])
-            data["id"] = url + f'/author/{author_id}/posts/{post_id}'
-            data["author"] = {
-                "type": author.type,
-                "id": str(author.id),
-                "host": author.host,
-                "displayName": author.displayName,
-                "url": author.url,
-                "github": f'http://github.com/{author.githubName}',
-                "profileImage": author.profileImage
-                    }
-            return Response(data, status = status.HTTP_200_OK)
-        elif post_id is None:
-            data = {'error': 'PUT request requires a post_id'}
-            return Response(data, status = status.HTTP_405_METHOD_NOT_ALLOWED)
-
-    elif request.method == 'DELETE':
-        if post_id is not None:
-            author = Author.objects.get(id=author_id)
-            post = Post.objects.get(id=post_id, author=author)
-            serializer = PostSerializer(post)
-            post.delete()
-            data = {"message" : "Deletion Successful"}
-            return Response(data, status = status.HTTP_202_ACCEPTED)
-        elif post_id is None:
-            data = {'error': 'DELETE request requires a post_id'}
-            return Response(data, status = status.HTTP_405_METHOD_NOT_ALLOWED)  
-
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors)
+    
+class PostDetails(APIView):
+    def get(self, request, author_id, post_id):
+        """
+        Retrieve a post.
+        """
+        author = Author.objects.get(pk=author_id)
+        try:
+            post = Post.objects.get(pk=post_id, author=author, visibility="PUBLIC")
+        except:
+            return Response({"error": "Post Not Found"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+    
+    def post(self, request, author_id, post_id):
+        """
+        Update a post.
+        """
+        # TODO: Implement authentication for this endpoint
+        author = Author.objects.get(pk=author_id)
+        post = Post.objects.get(pk=post_id, author=author)
+        serializer = PostSerializer(post, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    
+    def put(self, request, author_id, post_id):
+        """
+        Creates a post with a specific id.
+        """
+        if Post.objects.filter(pk=post_id).exists():
+            return Response({"error": "A post already exists with the given id"}, status=status.HTTP_409_CONFLICT)
+        
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(uuid=post_id)
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors)
+    
+    def delete(self, request, author_id, post_id):
+        """
+        Delete a post.
+        """
+        author = Author.objects.get(pk=author_id)
+        post = Post.objects.get(pk=post_id, author=author)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
