f1eb7139d0e6602eb87c3dac41d69d21b03d1547
Gary Ng Kwong Sang <gng4820@gmail.com>
2023-10-27 19:58:26 -0600

    Reset DB password to default

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "web-database",
         "USER": "postgres",
-        "PASSWORD": "Sparsh28",
+        "PASSWORD": "12345",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
