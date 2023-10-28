e053e0115e8dcc23789c3b7dc3d66697b9c17d85
sparsh_thakur <113547853+skullemote@users.noreply.github.com>
2023-10-28 16:34:22 -0600

    comments app created and models added

--- backend/comments/models.py
+++ backend/comments/models.py
@@ -6,10 +6,20 @@ from authors.models import Author
 
 # Create your models here.
 class Comment(models.Model):
+    CT_MARKDOWN = 'text/markdown'
+    CT_PLAIN = 'text/plain'
+    CONTENT_TYPE_CHOICES = [
+        (CT_MARKDOWN, 'Markdown'),
+        (CT_PLAIN, 'Plain Text')
+    ]
     type = models.CharField(max_length=300, blank=True, default="comment")
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     author = models.ForeignKey(Author, on_delete=models.PROTECT)
     comment = models.TextField()
-    contentType = models.CharField(max_length=300, default="text/plain")
+    contentType = models.CharField(
+        max_length=200,
+        choices=CONTENT_TYPE_CHOICES,
+        default=CT_MARKDOWN
+        )
     published = models.DateTimeField(auto_now_add=True, default=timezone.now)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
