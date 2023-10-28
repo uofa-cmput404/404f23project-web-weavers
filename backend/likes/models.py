34c969b03d573a4adce693ea78066e78a5c5528c
Gary Ng Kwong Sang <gng4820@gmail.com>
2023-10-28 16:45:16 -0600

    Created Like Model and migrations

--- backend/likes/models.py
+++ backend/likes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Like(models.Model):
+    context = models.CharField(max_length=200, default="https://www.w3.org/ns/activitystreams", name="@context")
+    summary = models.CharField(max_length=200)
+    type = models.CharField(max_length=50, default="Like")
+    author = models.ForeignKey("authors.Author", on_delete=models.CASCADE)
+    object = models.URLField(max_length=200)
+
