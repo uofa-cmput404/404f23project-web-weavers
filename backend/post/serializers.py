bbbd8cb40bec051be4bc182105b709311b4c9b15
Gary Ng Kwong Sang <gng4820@gmail.com>
2023-10-27 19:59:19 -0600

    Update serializers

--- backend/post/serializers.py
+++ backend/post/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 from .models import Post
+from authors.serializers import AuthorSerializer
 
 class PostSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer(read_only=True)
+
     class Meta:
         model = Post
-        fields = '__all__'
+        exclude = ['uuid']
