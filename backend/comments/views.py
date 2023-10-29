653e26ae089d59e16cf353a65b8c441bf6b573b4
sparsh_thakur <113547853+skullemote@users.noreply.github.com>
2023-10-29 03:08:56 -0600

    Update backend/comments/views.py
    Co-authored-by: Gary Ng Kwong Sang <51041584+garynks@users.noreply.github.com>

--- backend/comments/views.py
+++ backend/comments/views.py
@@ -21,7 +21,7 @@ class CommentList(APIView, PageNumberPagination):
 
         author = Author.objects.get(pk=author_id)
         post = Post.objects.get(pk=post_id)
-        comments = Comment.objects.filter(author=author,post=post).order_by('-published')
+        comments = Comment.objects.filter(author=author,post=post)
 
         if self.get_page_number(request, self):
             if self.get_page_size(request):
