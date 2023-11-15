a2ee28473b60ef71153c3194856620737fcf75fc
Genna-C <genna@ualberta.ca>
2023-11-15 00:31:26 -0700

    Add follow request choice

--- frontend/social-distribution/src/components/shadedClickableBox.js
+++ frontend/social-distribution/src/components/shadedClickableBox.js
@@ -1,19 +1,24 @@
 import { Avatar, Link, Flex, IconButton, Text, Modal } from "@chakra-ui/react";
 import { colors, spacing } from "../utils/theme";
-import {FiBell} from 'react-icons/fi'
+import {FiBell, FiUserCheck, FiUserMinus} from 'react-icons/fi'
+
 
 
 export default function ShadedClickableBox({variant_,text,username,avatar,...props}) {
     const variant = {
         notif: variant_ === 'notif',
         msg: variant_ === 'msg',
+        request: variant_ == 'request',
       }
 
     const handleClick = () => {
         console.log('clicked')
     }
+    const handleAcceptFollower = () =>{
+        console.log("accepting a new follower")
+    }
 
-     
+
     return(
         <Link
                     onClick={handleClick}
@@ -21,19 +26,49 @@ export default function ShadedClickableBox({variant_,text,username,avatar,...pro
                     }
                 >
         <Flex p={spacing.lg} style={styles.square} flexDir='row'mb={1}>
-            
-            {variant.notif ? 
+
+            {variant.notif ?
                 <Flex >
                     <IconButton mr={5} justifyItems={'left'} icon={<FiBell/>}>
                     </IconButton>
-                    <Text align={"left"}> 
+                    <Text align={"left"}>
                         {text}
                     </Text>
                 </Flex>
-            : 
-            
+            : variant.request ?
+
+            <Flex >
+                    <Flex style={styles.container} flexDir="column" align="right">
+
+                        <Flex flexDir="row" align={"center"}>
+                            <Avatar name={username} src={avatar} size="md" ml={2}/>
+                            <Text ml={5} align={"left"}> {username} would like to follow you </Text>
+                        </Flex>
+                        <Flex flexDir="row" align={"center"}>
+                            <Flex style={styles.buttons}>
+                            <IconButton
+                            aria-label="Comment"
+                            icon={<FiUserCheck />}
+                            onClick={handleAcceptFollower}
+                            />
+                            <Text marginTop="10px" as='i' ml={4} align={"center"}> Accept</Text>
+
+                            </Flex>
+                            <Flex style={styles.buttons}>
+                            <IconButton
+                            aria-label="Comment"
+                            icon={<FiUserMinus />}
+                            onClick={handleAcceptFollower}
+                            />
+                            <Text marginTop="10px" as='i' ml={4} align={"bottom"}> Decline</Text>
+                            </Flex>
+                    </Flex>
+                    </Flex>
+                </Flex>
+            :
+
             <Flex align ="center" style={styles.square} >
-                
+
                     <Flex style={styles.container} flexDir="row" align="right">
                         <Avatar name={username} src={avatar} size="md" ml={2}/>
                         <Flex flexDir="column" align={"left"}>
@@ -41,14 +76,14 @@ export default function ShadedClickableBox({variant_,text,username,avatar,...pro
                             <Text ml={5} align={"left"}> Hi! </Text>
                         </Flex>
                     </Flex>
-                    
+
                 </Flex>
-            
-            
-            } 
+
+
+            }
             </Flex>
             </Link>
-        
+
 
     )
 }
@@ -59,5 +94,9 @@ const styles = {
         // overflow: "auto",
         borderRadius: "5px",
         // padding: spacing.lg,
-    }
+    },
+    buttons: {
+        marginTop: "10px",
+        marginRight: "10px",
+      },
 }
--- frontend/social-distribution/src/pages/main/inbox_screens/messages.js
+++ frontend/social-distribution/src/pages/main/inbox_screens/messages.js
@@ -4,24 +4,24 @@ import { colors } from "../../../utils/theme";
 import ShadedClickableBox from "../../../components/shadedClickableBox";
 
 
-export default function Messages() { 
-    
+export default function Messages() {
+
     const users = [
         {fullName: "Jane Doe", username: "janedoe", avatar: "https://bit.ly/tioluwani-kolawole"},
         {fullName: "Kent Dodds", username: "kentcdodds", avatar: "https://bit.ly/kent-c-dodds"},
         {fullName: "Ryan Florence", username: "ryanflorence", avatar: "https://bit.ly/ryan-florence"},
-        {fullName: "Prosper Otemuyiwa", username: "unicodeveloper", avatar: "https://bit.ly/prosper-baba"}, 
-        {fullName: "Christian Nwamba", username: "codebeast", avatar: "https://bit.ly/code-beast"}, 
-        {fullName: "Segun Adebayo", username: "thesegunadebayo", avatar: "https://bit.ly/sage-adebayo"},   
+        {fullName: "Prosper Otemuyiwa", username: "unicodeveloper", avatar: "https://bit.ly/prosper-baba"},
+        {fullName: "Christian Nwamba", username: "codebeast", avatar: "https://bit.ly/code-beast"},
+        {fullName: "Segun Adebayo", username: "thesegunadebayo", avatar: "https://bit.ly/sage-adebayo"},
     ]
 
     return(
         <Flex flexDir="column" style={styles.container}>
             {users.map((user) => (
-                <ShadedClickableBox username={user.username} avatar={user.avatar} />
+                <ShadedClickableBox username={user.username} avatar={user.avatar} variant_='request' />
             ))}
-              
-        </Flex> 
+
+        </Flex>
     )
 
 }
