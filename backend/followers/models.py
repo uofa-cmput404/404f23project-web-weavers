edfb2620daf599f7d8348d88294375a62524d772
Gary Ng Kwong Sang <gng4820@gmail.com>
2023-11-15 15:49:43 -0700

    Add Follow Requests

--- backend/followers/models.py
+++ backend/followers/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Follow(models.Model):
     type = models.CharField(max_length=50, default="Follow")
     summary = models.CharField(max_length=200)
-    actor = models.ForeignKey("authors.Author", on_delete=models.CASCADE) # Person sending the Follow request
-    object = models.JSONField() # Person receiving the Follow request
+    actor = models.ForeignKey("authors.Author", related_name='follow_requester', on_delete=models.CASCADE) # Person sending the Follow request
+    object = models.ForeignKey("authors.Author", related_name='author_to_follow', on_delete=models.CASCADE) # Person receiving the Follow request
--- backend/followers/serializers.py
+++ backend/followers/serializers.py
@@ -3,7 +3,8 @@ from .models import Follow
 from authors.serializers import AuthorSerializer
 
 class FollowSerializer(serializers.ModelSerializer):
-    author = AuthorSerializer(read_only=True)
+    actor = AuthorSerializer(read_only=True)
+    object = AuthorSerializer(read_only=True)
 
     class Meta:
         model = Follow
