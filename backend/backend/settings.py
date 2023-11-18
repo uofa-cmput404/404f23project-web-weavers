db62e173be7d20221ed940f875e941f4b7f93ba1
Gary Ng Kwong Sang <gng4820@gmail.com>
2023-11-18 16:31:54 -0700

    Remove changes not necessary for backend deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -17,7 +17,6 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-ROOT_DIR = os.path.dirname(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -138,9 +137,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # Django static files (Django admin page, etc.)
-STATICFILES_DIRS = [
-    os.path.join(ROOT_DIR, 'frontend/social-distribution/build/static') # React static files (React frontend)
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
