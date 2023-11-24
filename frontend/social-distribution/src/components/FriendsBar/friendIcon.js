31556de59956423104928cbaf7adc52cfeed45fb
jazarcon <jd.azarcon3@gmail.com>
2023-11-23 22:28:11 -0700

    changing format for the follow button

--- frontend/social-distribution/src/components/FriendsBar/friendIcon.js
+++ frontend/social-distribution/src/components/FriendsBar/friendIcon.js
@@ -1,11 +1,13 @@
 import {colors} from "../../utils/theme.js";
-import { Avatar, Flex, Link, Text } from "@chakra-ui/react";
+import { Avatar, Collapse, Flex, Link, Text, Button, useDisclosure, IconButton } from "@chakra-ui/react";
 import React from "react";
 import { useNavigate } from 'react-router-dom';
+import { AiOutlineComment } from "react-icons/ai";
 
 
 export default function FriendIcon({user, ...props}){
    const {fullName, username, avatar} = user;
+   const {isOpen, onToggle}= useDisclosure();
    
     let navigate = useNavigate();
     const handleClick = () => {
@@ -17,12 +19,20 @@ export default function FriendIcon({user, ...props}){
             <Link
                 _hover={{ textDecor: 'none',  color: colors.brand.c1, backgroundColor: 'transparent' }}
                 alignItems="left"
-                onClick={handleClick}
+                // onClick={handleClick}
             >
-                <Flex style={styles.container} flexDir="row" align="right">
+                <Flex style={styles.container} flexDir="row" align="right" onClick={onToggle}>
                     <Avatar name={username} src={avatar} size="md" ml={2}/>
                     <Text ml={5} mt={4} fontSize={14}> {username} </Text>
                 </Flex>
+                <Collapse in={isOpen} animateOpacity> 
+                    <IconButton
+                        aria-label="Profile"
+                        icon={<AiOutlineComment />}
+                        onClick={handleClick}   
+                    />
+                    <Button>Follow</Button>
+                </Collapse>
             </Link>
         </Flex>
     )
