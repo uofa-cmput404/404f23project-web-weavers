0cd862dcbe98c763a941e33a1493bf63aa609771
Gary Ng Kwong Sang <gng4820@gmail.com>
2023-11-25 16:26:54 -0700

    Minor corrections

--- backend/authors/serializers.py
+++ backend/authors/serializers.py
@@ -4,4 +4,4 @@ from .models import Author
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
-        exclude = ['followers', 'inbox', 'is_superuser', "user_permissions", "groups", 'is_staff', "password", "email", "last_login",]
+        exclude = ['followers', 'is_superuser', "user_permissions", "groups", 'is_staff', "password", "email", "last_login",]
