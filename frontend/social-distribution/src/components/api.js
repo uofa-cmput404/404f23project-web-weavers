7887ece755f0bfa218d8da5a98e4763a332f2fd9
Genna-C <genna@ualberta.ca>
2023-11-27 03:25:41 -0700

    reset back to local

--- frontend/social-distribution/src/components/api.js
+++ frontend/social-distribution/src/components/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import axiosService from '../utils/axios';
-export const API_URL = "https://web-weavers-backend-fb4af7963149.herokuapp.com/";
+export const API_URL = "http://127.0.0.1:8000/";
 
 function validateUser (username) {
     //Checks if a User exists in the database
