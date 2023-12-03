fed26fe3fff906479ccbd624eb5b13debb8c1c03
jazarcon <jd.azarcon3@gmail.com>
2023-12-02 22:59:12 -0700

    converting my stream to a profile page

--- frontend/social-distribution/src/pages/main/mystream.js
+++ frontend/social-distribution/src/pages/main/mystream.js
@@ -9,25 +9,35 @@ import Post from "../../components/Posts/Posted";
 import {getDisplayName} from "../../components/api";
 import axiosService from "../../utils/axios";
 import { useState, useEffect } from 'react';
+import { Avatar, Flex } from "@chakra-ui/react";
 
 export default function MyStream({props}){
     const user = localStorage.getItem("user")
     // need a list of all postIDS to show
     const [posts, setPosts] = useState([]);
+    const [followers, setFollowers] = useState([]);
+    const [friends, setFriends] = useState([]);
+    const [data, setData] = useState([]); // [postID, postID, ...
     const [displayName, setDisplayName] = useState("");
 
     //This queries the user for all personal posts
     const fetchdata = async () => {
         const res = await axiosService.get("authors/" + user + "/posts/")
         setPosts(res.data.items)
-        const res2 = await getDisplayName(user)
+        
+        const res2 = getDisplayName(user)
         setDisplayName(res2);
+        
+        const res3 = await axiosService.get("authors/" + user + "/followers/")
+        setFollowers(res3.data.items)
+
     };
     useEffect(() => {
         fetchdata();
     }, [])
 
-
+    console.log("user " + user);
+    console.log("displayName " + displayName);
 
 
     return(
@@ -35,7 +45,19 @@ export default function MyStream({props}){
             <LogoBar/>
             <NavBar current='My Stream'/>
             <FriendsBar/>
-            <h1>My Stream</h1>
+            <Flex flexDir="column" style={styles.profileHeader}>
+                <Avatar size="2xl" name={displayName} src="https://bit.ly/tioluwani-kolawole" />
+                <h1>UserJK</h1>
+                {/* <Flex fontSize={sizes.xl} color="black" flexDir="row">
+                    
+                </Flex> */}
+                
+                <Flex flexDir="row" justifyContent="stretch" alignItems="center" fontSize={sizes.sm} color="black">
+                    <div>Posts {posts.length}</div>
+                    <div>Followers {followers.length}</div>
+                    <div>Friends</div>
+                </Flex>
+            </Flex>
             <div style={styles.content}>
                 <div style={{ ...styles.postContainer }}>
                     {/* TODO: change this to be more dynamic when pulling list of posts */}
@@ -53,7 +75,21 @@ const styles = {
     container:{
         backgroundColor: colors.brand.c6,
         height: "100vh",
-
+        paddingTop: spacing.md,
+    },
+    profileHeader: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        backgroundColor: colors.brand.c3,
+        paddingBottom: spacing.md,
+        overflow: "auto",
+        paddingTop: spacing.lg + "5vh",
+        marginBottom: spacing.md,
+        width: '50%', // specify the width
+        marginLeft: 'auto', // center the element
+        marginRight: 'auto', // center the element
+        borderRadius: '10px', // round the corners
     },
     postContainer: {
         display: "flex",
