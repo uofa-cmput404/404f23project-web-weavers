641ac8c19755d3e42bcbcfb5fc2f34ba3283ef9a
Gary Ng Kwong Sang <gng4820@gmail.com>
2023-12-04 13:39:09 -0700

    Add utility functions for retrieving remote authors and posts

--- backend/inbox/views.py
+++ backend/inbox/views.py
@@ -2,7 +2,6 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.pagination import PageNumberPagination
-from authors.serializers import AuthorSerializer
 from post.serializers import PostSerializer
 from likes.serializers import LikeSerializer
 from followers.serializers import FollowSerializer
@@ -17,8 +16,7 @@ from likes.models import Like
 from comments.models import Comment
 from nodes.permissions import IsAuthorizedNode
 from drf_spectacular.utils import extend_schema
-import requests
-from requests.auth import HTTPBasicAuth
+from backend.utils import get_remote_author, get_remote_post
 
 # Create your views here.
 @extend_schema(
@@ -154,7 +152,7 @@ class InboxView(APIView, PageNumberPagination):
             try:
                 follow_requester = Author.objects.get(id=request.data["actor"])
             except Author.DoesNotExist:
-                follow_requester = self._get_remote_author(request.data["actor"])
+                follow_requester = get_remote_author(request.data["actor"])
                 
                 # if an issue occurred while getting the remote author, return the error
                 if isinstance(follow_requester, Response):
@@ -179,7 +177,7 @@ class InboxView(APIView, PageNumberPagination):
             try:
                 like_sender = Author.objects.get(id=request.data["author"])
             except Author.DoesNotExist:
-                like_sender = self._get_remote_author(request.data["author"])
+                like_sender = get_remote_author(request.data["author"])
 
                 # if an issue occurred while getting the remote author, return the error
                 if isinstance(like_sender, Response):
@@ -211,7 +209,7 @@ class InboxView(APIView, PageNumberPagination):
             try: 
                 post = Post.objects.get(id=request.data["id"])
             except Post.DoesNotExist:
-                post = self._get_remote_post(request.data["id"])
+                post = get_remote_post(request.data["id"])
 
                 # if an issue occurred while getting the remote post, return the error
                 if isinstance(post, Response):
@@ -231,82 +229,6 @@ class InboxView(APIView, PageNumberPagination):
             return Response(comment_serializer.data, status = status.HTTP_200_OK)
 
         return Response(status = status.HTTP_400_BAD_REQUEST)
-    
-    def _get_remote_author(self, remote_author_url):
-        if remote_author_url.startswith("https://c404-5f70eb0b3255.herokuapp.com"):
-            headers = {'Authorization': 'Bearer 06c591151b14e0462efd2ad9c91888a530967c7f'} 
-        elif remote_author_url.startswith("https://beeg-yoshi-backend-858f363fca5e.herokuapp.com"):
-            headers = {'Authorization': 'Token bcad92d727cc40cd0435370dd285f9b82626890b'}
-        elif remote_author_url.startswith("https://packet-pirates-backend-d3f5451fdee4.herokuapp.com"):
-            remote_author = requests.get(remote_author_url, auth=HTTPBasicAuth('WebWeavers', '12345')).json()
-        else:
-            headers = {}
-
-        # For teams using Django, it will be necessary to add a trailing slash to the URL
-        if not remote_author_url.startswith("https://packet-pirates-backend-d3f5451fdee4.herokuapp.com"):
-            if remote_author_url.endswith("/"):
-                remote_author = requests.get(remote_author_url, headers).json()
-                remote_author_url = remote_author_url[:-1]
-            else:
-                remote_author = requests.get(remote_author_url + "/", headers).json()
-
-        # if the author does not exist, create it
-        if not Author.objects.filter(displayName=remote_author["displayName"], host=remote_author["host"]).exists():
-            author_serializer = AuthorSerializer(data=remote_author)
-            if author_serializer.is_valid():
-                # if the author's id is not a URL, then it is a ID
-                if not remote_author["id"].startswith("http"):
-                    author_serializer.validated_data["uuid"] = remote_author["id"]
-                else:
-                    # extract uuid from the URL
-                    author_serializer.validated_data["uuid"] = remote_author["id"].split("/")[-1]
-
-                author_serializer.validated_data["id"] = remote_author_url
-                author_serializer.validated_data["url"] = remote_author_url
-                author_serializer.validated_data["host"] = remote_author["host"]
-                return author_serializer.save()
-            else:
-                return Response(author_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        else:
-            return Author.objects.get(displayName=remote_author["displayName"], host=remote_author["host"])
-    
-    def _get_remote_post(self, remote_post_url):
-        if remote_post_url.startswith("https://c404-5f70eb0b3255.herokuapp.com"):
-            headers = {'Authorization': 'Bearer 06c591151b14e0462efd2ad9c91888a530967c7f'} 
-        elif remote_post_url.startswith("https://beeg-yoshi-backend-858f363fca5e.herokuapp.com"):
-            headers = {'Authorization': 'Token bcad92d727cc40cd0435370dd285f9b82626890b'}
-        elif remote_post_url.startswith("https://packet-pirates-backend-d3f5451fdee4.herokuapp.com"):
-            remote_post = requests.get(remote_post_url, auth=HTTPBasicAuth('WebWeavers', '12345')).json()
-        else:
-            headers = {}
-
-        # For teams using Django, it will be necessary to add a trailing slash to the URL
-        if not remote_post_url.startswith("https://packet-pirates-backend-d3f5451fdee4.herokuapp.com"):
-            if remote_post_url.endswith("/"):
-                remote_post = requests.get(remote_post_url, headers).json()
-                remote_post_url = remote_post_url[:-1]
-            else:
-                remote_post = requests.get(remote_post_url + "/", headers).json()
-
-
-        # if the post does not exist, create it
-        if not Post.objects.filter(id=remote_post["id"]).exists():
-            post_serializer = PostSerializer(data=remote_post)
-            if post_serializer.is_valid():
-
-                remote_author = self._get_remote_author(remote_post["author"]["url"])
-
-                # if an issue occurred while getting the remote author, return the error
-                if isinstance(remote_author, Response):
-                    return remote_author
-
-                post_serializer.validated_data["id"] = remote_post["id"]
-                post_serializer.validated_data["author"] = remote_author
-                return post_serializer.save()
-            else:
-                return Response(post_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        else:
-            return Post.objects.get(id=remote_post["id"])
 
     @extend_schema(
         description="Clear the author's inbox.",
