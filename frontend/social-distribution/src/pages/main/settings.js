8bbe946931333bf08d221f89bbd65cd6e8720060
jazarcon <jd.azarcon3@gmail.com>
2023-12-07 20:52:47 -0700

    Inbox + Edit Profile
    Messages --> Comments
    Can change Username and Github

--- frontend/social-distribution/src/pages/main/settings.js
+++ frontend/social-distribution/src/pages/main/settings.js
@@ -1,19 +1,41 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import NavBar from "../../components/Bars/navbar.js";
 import { colors, sizes } from "../../utils/theme.js";
 import LogoBar from "../../components/Bars/logoBar.js";
 import FriendsBar from "../../components/FriendsBar/friendsBar.js";
 import { Card, CardBody, Divider } from "@chakra-ui/react";
 import axiosService from '../../utils/axios.ts';
-import { FontSize } from '../../components/OtherServers/styles.js';
+import Button from '../../components/Button.js';
 
 export default function Settings({props}){
     const user = localStorage.getItem("user")
     const [username, setUsername] = useState("")
-    const [profilePicture, setProfilePicture] = useState(null)
+    const [profilePicture, setProfilePicture] = useState("")
+    const [gitHub, setGitHub] = useState("")
+    const [isLoading, setIsLoading] = useState(false)
+    const [isSubmitted, setIsSubmitted] = useState(false)
+    const [isLoadingUser, setIsLoadingUser] = useState(true)
+
+    useEffect(() => {
+        const fetchdata = async () => {
+            // takes a second to load this
+            const res = await axiosService.get("authors/" + user)
+            setUsername(res.data.displayName)
+            setGitHub(res.data.github)
+            setProfilePicture(res.data.profileImage)
+        };
+        fetchdata()
+        setIsLoadingUser(false)
+    }
+    , [])
+
 
     const handleUsernameChange = (event) => {
         setUsername(event.target.value)
+    }
+
+    const handleGitHubChange = (event) => {
+        setGitHub(event.target.value)
     }
 
     const handleProfilePictureChange = (event) => {
@@ -25,15 +47,17 @@ export default function Settings({props}){
     }
 
     const handleSubmit = (event) => {
+        setIsLoading(true)
         event.preventDefault();
 
         const res = axiosService.post("authors/" + user + "/", {
             displayName: username,
-            profilePicture: profilePicture
+            profilePicture: profilePicture,
+            github: gitHub
         })
 
-
-
+        setIsLoading(false)
+        setIsSubmitted(true)
     }
 
     return(
@@ -46,26 +70,32 @@ export default function Settings({props}){
                 <CardBody >
                     <h1 style={{fontSize: sizes.lg}}>Settings</h1>
                     <Divider/>
-                    <form onSubmit={handleSubmit} style={styles.editForm}>
-                        <label style={{marginBottom: '20px'}}>
-                            Username: 
-                            <input type="text" value={username} onChange={handleUsernameChange} />
-                        </label>
-                        <label style={{marginBottom: '20px'}}>
-                            Password: 
-                            <input type="password" />
-                        </label>
-                        <label style={{marginBottom: '20px'}}>
-                            Profile Picture: 
-                            <input type="file" onChange={handleProfilePictureChange} />
-                        </label>
-                        <label style={{marginBottom: '20px'}}>
-                            GitHub Account: 
-                            <input type="text" />
-                        </label>
+                    {!isLoadingUser && (
+                        <form onSubmit={handleSubmit} style={styles.editForm}>
+                            <label style={{display: 'flex', justifyContent: 'space-between', width: '100%', marginBottom: '20px'}}>
+                                Username: 
+                                <input type="text" value={username} style={{marginLeft: '10px'}} onChange={handleUsernameChange} />
+                            </label>
+                            <label style={{display: 'flex', justifyContent: 'space-between', width: '100%', marginBottom: '20px'}}>
+                                GitHub Account: 
+                                <input type="text" value={gitHub} style={{marginLeft: '10px'}} onChange={handleGitHubChange}/>
+                            </label>                        
+                            <label style={{...styles.buttonIcon, cursor: 'pointer'}}>
+                                <input type="file" value={profilePicture} style={{display: 'none', marginLeft: '10px'}} onChange={handleProfilePictureChange} />
+                                Choose File
+                            </label>
+                            <Button 
+                                type="submit" 
+                                isLoading={isLoading} 
+                                style={styles.buttonIcon}>
+                                    {isLoading ? "Loading..." : "Submit"}
+                            </Button>
+                            {isSubmitted && (
+                                <div style={{color: 'green', marginTop: '10px'}}>Submitted!</div>
+                            )}
+                        </form>
+                    )}
 
-                        <input type="submit" value="Submit" />
-                    </form>
                 </CardBody>
             </Card>
 
@@ -89,8 +119,17 @@ const styles = {
         display: 'flex', 
         flexDirection: 'column', 
         alignItems: 'center', 
-        backgroundColor: colors.brand.c4,
+        // backgroundColor: colors.brand.c4,
         padding: '20px',
         borderRadius: '10px',
+        height: '80%',
+    },
+    buttonIcon:{
+        marginTop: '1rem',
+        backgroundColor: colors.brand.c2,
+        color: 'white',
+        padding: '10px 20px',
+        borderRadius: '1rem',
+        boxShadow: '0 0 1rem #0001',
     }
 }
